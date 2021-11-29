0eb4b70eee338f41e98f007a610c40882be65812
Tesfa-Worku <hopegorg@gmail.com>
2021-11-29 17:10:10 -0500

    chore(list container) construct list container #2

--- decidr/src/App.js
+++ decidr/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState} from 'react';
 
 function App() {
+  const [listContainer, setListContainer] = useState([])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p>Decidr App</p>
       </header>
     </div>
   );
