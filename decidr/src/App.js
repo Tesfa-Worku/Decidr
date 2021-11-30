361b4d0ba5f6b401f229b7dd34dcce1b6bbd9cfd
Tesfa-Worku <hopegorg@gmail.com>
2021-11-30 13:43:26 -0500

    refactor(Decidr): add header #13

--- decidr/src/App.js
+++ decidr/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Decidr</h1>
       </header>
       <body className="Input">
         <div>
