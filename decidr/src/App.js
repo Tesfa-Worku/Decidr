0c332f0de0bec6b02aedd2806c312092442bdc1f
Tesfa-Worku <hopegorg@gmail.com>
2021-11-30 22:24:22 -0500

    chore(container): add list container state #2

--- decidr/src/App.js
+++ decidr/src/App.js
@@ -1,28 +1,8 @@
-import './App.css';
-import { useState} from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [listContainer, setListContainer] = useState([]);
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Decidr</h1>
-      </header>
-      <body className="Input">
-        <div>
-          <input type="text" name="Add Item" placeholder="Add Item"/>
-          <button 
-          // onChange={}
-          >
-            Add Item</button>
-        </div>
-        <div>
-          <button>Select An Item</button>
-        </div>           
-      </body>
-    </div>
-  );
 }
 
 export default App;
